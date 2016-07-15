948ddd79d891bba71d1ed0ad71c0b8d59063d944
GrassrootsWorkers <494238284@qq.com>
2016-07-15 15:57:11 +0800

    22016年7月15号

---
+++ fruit-interfaces/src/main/java/com/farmer/fruit/models/address/Address.java
@@ -0,0 +1,43 @@
+package com.farmer.fruit.models.address;
+
+/**
+ * Created by liuzhi on 2016/7/1.
+ */
+public class Address {
+    private Long id;
+    private String name;
+    private String type;
+    private Long parentId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Long getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Long parentId) {
+        this.parentId = parentId;
+    }
+}
