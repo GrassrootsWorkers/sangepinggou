30a275e186089009f7db830a0ac593a85f7369e9
liuzhi <liuzhi@missfresh.cn>
2016-11-20 11:14:25 +0800

    数据库备份

--- fruit-commons/src/main/java/com/farmer/fruit/utils/MD5Util.java
+++ fruit-commons/src/main/java/com/farmer/fruit/utils/MD5Util.java
@@ -1,5 +1,10 @@
 package com.farmer.fruit.utils;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Formatter;
+
 /**
  * Created by liuzhi on 2016/6/26.
  */
@@ -15,17 +20,45 @@ public class MD5Util {
     public static String entryptPassword(String plainPassword) {
         byte[] salt = Digests.generateSalt(SALT_SIZE);
         byte[] hashPassword = Digests.sha1(plainPassword.getBytes(), salt, HASH_INTERATIONS);
-        return Encodes.encodeHex(salt)+Encodes.encodeHex(hashPassword);
+        return Encodes.encodeHex(salt) + Encodes.encodeHex(hashPassword);
     }
+
     /**
      * 验证密码
+     *
      * @param plainPassword 明文密码
-     * @param password 密文密码
+     * @param password      密文密码
      * @return 验证成功返回true
      */
     public static boolean validatePassword(String plainPassword, String password) {
-        byte[] salt = Encodes.decodeHex(password.substring(0,16));
+        byte[] salt = Encodes.decodeHex(password.substring(0, 16));
         byte[] hashPassword = Digests.sha1(plainPassword.getBytes(), salt, HASH_INTERATIONS);
-        return password.equals(Encodes.encodeHex(salt)+Encodes.encodeHex(hashPassword));
+        return password.equals(Encodes.encodeHex(salt) + Encodes.encodeHex(hashPassword));
+    }
+
+    /**
+     * 微信 ticket 签名
+     * @param signString
+     * @return
+     * @throws NoSuchAlgorithmException
+     * @throws UnsupportedEncodingException
+     */
+    public static String sign(String signString) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        MessageDigest crypt = MessageDigest.getInstance("SHA-1");
+        crypt.reset();
+        crypt.update(signString.getBytes("UTF-8"));
+        String signature = byteToHex(crypt.digest());
+        return signature;
+    }
+
+
+    private static String byteToHex(final byte[] hash) {
+        Formatter formatter = new Formatter();
+        for (byte b : hash) {
+            formatter.format("%02x", b);
+        }
+        String result = formatter.toString();
+        formatter.close();
+        return result;
     }
 }
